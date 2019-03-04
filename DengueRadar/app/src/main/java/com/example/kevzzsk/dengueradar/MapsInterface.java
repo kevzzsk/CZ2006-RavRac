e45a4af0ab1719e7690db84c3e3ac63fd3687648
KevinSk <kevi0023@e.ntu.edu.sg>
2019-03-04 23:46:15 +0800

    Working blank map app

--- DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MapsInterface.java
+++ DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MapsInterface.java
@@ -1,8 +1,21 @@
 package com.example.kevzzsk.dengueradar;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,7 +25,75 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsInterface extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsInterface";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
     private GoogleMap mMap;
+
+
+
+
+    public boolean isServicesOK(){
+        Log.d(TAG,"isServicesOK: Checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapsInterface.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            // everything is fine
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            // an error occur but can be resolved
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+
+            Dialog dialog =GoogleApiAvailability.getInstance().getErrorDialog(MapsInterface.this,available,ERROR_DIALOG_REQUEST);
+            ((Dialog) dialog).show();
+
+        }else{
+            Toast.makeText(this, "you cannot make map request",Toast.LENGTH_SHORT).show();
+        }
+        return false;
+
+    }
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private boolean mLocationPermissionGranted = false;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private void getLocationPermission(){
+        String[] permission = {Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)==PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            }else {
+                ActivityCompat.requestPermissions(this,permission,LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:
+                if(grantResults.length > 0){
+                    for (int i=0 ; i<grantResults.length; i++){
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    //init map
+                }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,4 +124,5 @@ public class MapsInterface extends FragmentActivity implements OnMapReadyCallbac
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+    // api key AIzaSyC023WrXGzzcNAY3yoAXj1iT2cRnEypGr8
 }
