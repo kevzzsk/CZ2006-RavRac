187a42c2dc1354b5dfe6141fee5348fc21ed18e5
KevinSk <kevi0023@e.ntu.edu.sg>
2019-03-21 09:02:35 +0800

    Add Working SearchBar

--- DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MapsInterface.java
+++ DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MapsInterface.java
@@ -2,11 +2,19 @@ package com.example.kevzzsk.dengueradar;
 
 import android.Manifest;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -14,21 +22,42 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.LocationRestriction;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteFragment;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
-public class MapsInterface extends Fragment implements OnMapReadyCallback {
+import java.util.Arrays;
+
+
+public class MapsInterface extends Fragment implements OnMapReadyCallback,GoogleMap.OnMarkerClickListener {
+    // use getActivity() to replace this as the context
+    // This is due to MapsInterface being just a fragment, hence context
+    // need to be taken from the "parent" (MenuInterface)
 
     GoogleMap mMap;
-    private static final String TAG = "MenuInterface";
+    private static final String TAG = "MapsInterface";
+
+    private static final LatLng sg = new LatLng(1.3521, 103.8198);
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -59,7 +88,6 @@ public class MapsInterface extends Fragment implements OnMapReadyCallback {
         return false;
 
     }
-
 
     private void getLocationPermission(){
         String[] permission = {Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION};
@@ -99,11 +127,11 @@ public class MapsInterface extends Fragment implements OnMapReadyCallback {
 
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.map_fragment,container,false);
-        Log.d("MapsInterface", "onCreateView: hi");
 
         return v;
     }
@@ -112,19 +140,81 @@ public class MapsInterface extends Fragment implements OnMapReadyCallback {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         // INIT MAP
-        getLocationPermission();
+        getLocationPermission(); // GET PERMISSION FOR LOCATION
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        displaySearchBar();
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sg = new LatLng(1.3521, 103.8198);
-        mMap.addMarker(new MarkerOptions().position(sg).title("Marker in Singapore"));
+        // Add a marker in Singapore and move the camera
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sg));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.setOnMarkerClickListener(this);
+
+    }
+
+    public void displaySearchBar(){
+        // search_Bar
+        if (!Places.isInitialized()) {
+            Places.initialize(getActivity(), "AIzaSyC023WrXGzzcNAY3yoAXj1iT2cRnEypGr8");
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.search_bar);
+
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG));
+
+        autocompleteFragment.setCountry("sg"); //restrict search to SG
+
+        autocompleteFragment.getView().setBackgroundColor(Color.parseColor("#45091F"));
+        EditText search_input = autocompleteFragment.getView().findViewById(R.id.places_autocomplete_search_input);
+        search_input.setTextColor(Color.WHITE);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                /*mMap.addMarker(new MarkerOptions()
+                        .position(place.getLatLng())
+                        .title(place.getName())
+                        .icon(bitmapDescriptorFromVector(getContext(),R.drawable.ic_pin)));*/
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(),12));
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId()+", " + place.getAddress());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Snackbar.make(getView(),"Error Occured",Snackbar.LENGTH_SHORT);
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        //remove marker on click
+        marker.remove();
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
+    }
+    // convert SVG to Bitmap for Gmap icon
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable background = ContextCompat.getDrawable(context, R.drawable.ic_pin);
+        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth() + 0, vectorDrawable.getIntrinsicHeight() + 0);
+        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        background.draw(canvas);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
--- DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MenuInterface.java
+++ DengueRadar/app/src/main/java/com/example/kevzzsk/dengueradar/MenuInterface.java
@@ -1,8 +1,11 @@
 package com.example.kevzzsk.dengueradar;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,10 +13,14 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MenuInterface extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG = "MenuInteface";
     private DrawerLayout drawer;
+
+
 
     @Override
     public void onBackPressed() {
@@ -50,11 +57,12 @@ public class MenuInterface extends AppCompatActivity implements NavigationView.O
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_interface);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Dengue Radar");
 
-        setContentView(R.layout.activity_maps_interface);
 
         drawer = findViewById(R.id.drawerLayout);
 
@@ -67,6 +75,7 @@ public class MenuInterface extends AppCompatActivity implements NavigationView.O
         Log.d(TAG, "onCreate: drawer message!"+ String.valueOf(drawer == null) );
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
 
 
         //set MapInterface to be default start up page
